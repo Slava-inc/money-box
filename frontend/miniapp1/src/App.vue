364e89ed72b72f53d482dd246d5526215ee92c93
Slava <danchenko57@gmail.com>
2025-10-03 09:54:23 +0300

    App.vue correction

--- frontend/miniapp1/src/App.vue
+++ frontend/miniapp1/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <v-app>
+    <v-main>
+      <h1>You did it!</h1>
+        Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+        documentation
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped></style>
